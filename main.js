0d355ba44fe7ac8c3e8c9c2d7722973b9372b5f9
helado de brownie <helado.de.brownie@gmail.com>
2020-04-02 23:19:18 -0400

    implement rgb to hsv conversion

--- main.js
+++ main.js
@@ -19,11 +19,37 @@ void function () {
     }
 
     function hsvColorFromRgbColor(rgbColor) {
-        // TODO
+        // Algorithm adapted from https://en.wikipedia.org/wiki/HSL_and_HSV#From_RGB
+
+        var red = rgbColor.red / 255
+        var green = rgbColor.green / 255
+        var blue = rgbColor.blue / 255
+
+        var value = Math.max(red, green, blue)
+        var chroma = value - Math.min(red, green, blue)
+
+        var hue =
+            chroma == 0     ? 0 :
+            value == red    ? 60 * (0 + (green - blue) / chroma) :
+            value == green  ? 60 * (2 + (blue - red) / chroma) :
+                              60 * (4 + (red - green) / chroma)
+
+        var saturation = value == 0 ? 0 : chroma / value
+
         return {
-            hue: 0,
-            saturation: 0,
-            value: 0,
+            hue:        normalizeAngle(hue),
+            saturation: saturation,
+            value:      value,
+        }
+    }
+
+    function normalizeAngle(degrees) {
+        if (degrees < 0) {
+            return normalizeAngle(degrees + 360)
+        } else if (degrees >= 360) {
+            return normalizeAngle(degrees - 360)
+        } else {
+            return degrees
         }
     }
 
